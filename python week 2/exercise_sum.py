cefccbbb454312f0408bf791c285ad5e3d631df5
lian-david <67974673+lian-david@users.noreply.github.com>
2022-07-26 16:41:08 -0400

    Delete python week 2 directory

--- python week 2/exercise_sum.py
+++
@@ -1,7 +0,0 @@
-#sum all the numbers that divide by 7 between 100 & 200
-
-total = 0 
-for i in range (100, 201):
-    if i % 7 == 0: 
-        total += i
-print("The sum is:", total)
--- python week 2/list_helper_main.py
+++
@@ -1,5 +0,0 @@
-import list_helper
-
-print(list_helper.get_range([1,2,3,4,8], 1, 4))
-print(list_helper.get_random_item([1,2,3]))
-print(list_helper.second_min([1,2,3,4,5,6]))
